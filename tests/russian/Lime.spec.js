c509ff249c93a53fe01a395dab1c275fda719aa5
RSashaQA <a.romanov.limehd@gmail.com>
2023-03-03 22:34:37 +0300

    adds some tests for lime

--- tests/russian/Lime.spec.js
+++ tests/russian/Lime.spec.js
@@ -37,3 +37,52 @@ test("visit limehd.tv website and check errors in console with logger", async ({
   await page.waitForTimeout(5000)
   //await page.screenshot({ path: "screenshots/limehd.tv.png" });
 })
+
+// test("visit limehd.tv/movies and check errors in console with logger", async ({ page }) => {
+
+//   logger(page)
+
+//   const response = await page.goto(process.env.ENVIRONMENT_URL || 'https://limehd.tv/movies');
+//   expect(response.status()).toBeLessThan(400);
+
+//   await expect(page).toHaveURL(/.*movies/)
+
+//   await page.waitForSelector('.slider__list')
+
+// })
+
+// test("visit limehd.tv/, login, then try to by subscribes with yoomoney", async ({ page }) => {
+
+//   //logger(page)
+
+//   const response = await page.goto(process.env.ENVIRONMENT_URL || 'https://limehd.tv/login');
+//   expect(response.status()).toBeLessThan(400);
+
+//   await page.getByPlaceholder('Введите e-mail').fill('228@322.com');
+
+//   await page.getByPlaceholder('Введите пароль').fill('qqqqqq');
+
+//   await page.getByRole("button", { name: 'Войти' }).click();
+
+//   const login_response = await page.waitForResponse('https://limehd.tv/api/v4/login');
+//   expect(login_response.status()).toBe(200);
+
+//   await page.getByRole('banner').getByRole('link', { name: 'Подписки' }).click();
+
+//   await page.getByRole('link', { name: 'ТВ Детский 79 Р/Мес' }).click();
+
+//   const t0 = Date.now();
+
+//   // page.on('request', req => { if (req.url().includes('yoomoney')) { console.log(`> ${Date.now() - t0} request start: ${req.url()}`) } })
+//   // page.on('response', req => { if (req.url().includes('yoomoney')) { console.log(`< ${Date.now() - t0} response: ${req.url()}`) } })
+//   // page.on('requestfinished', req => { if (req.url().includes('yoomoney')) { console.log(`. ${Date.now() - t0} request finished: ${req.url()}`) } })
+//   page.on('requestfailed', req => {
+//     if (req.url().includes('https://yoomoney.ru')) {
+//       throw new Error(`E request failed: ${req.url()}`)
+//     }
+//   })
+
+//   await page.getByRole('button', { name: 'Оформить подписку' }).click();
+
+//   await page.waitForTimeout(5000);
+// })
